f8865fa89b61be5878b99eb204738a7fe554e7e5
BenSeddikMehdi <amigoinpt@gmail.com>
2019-11-18 20:59:48 +0100

    Initial commit

---
+++ FirstProblem.h
@@ -0,0 +1,15 @@
+//
+// Created by HP-EliteBook on 11/18/2019.
+//
+
+#ifndef CNC_2012_FIRSTPROBLEM_H
+#define CNC_2012_FIRSTPROBLEM_H
+
+/* Question A-1 */
+/****************/
+char *S = "9876523047852309";
+int stringNumbers() {
+    
+}
+
+#endif //CNC_2012_FIRSTPROBLEM_H

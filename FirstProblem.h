60c11164096d60dc7733c5a4c2860d9fc85fb177
BenSeddikMehdi <amigoinpt@gmail.com>
2019-11-19 23:53:36 +0100

    deleteZeros

--- FirstProblem.h
+++ FirstProblem.h
@@ -11,33 +11,31 @@
 /* Question A-1 */
 /****************/
 //char S[] = "0000607";
-int stringNumbers(char S[]) {
-    for (int i = 0; S[i] != '\0' ; ++i) {
-        if (S[i] < '0' || S[i] > '9') return 0;
+int stringNumbers(const char string[]) {
+    for (int i = 0; string[i] != '\0' ; ++i) {
+        if (string[i] < '0' || string[i] > '9') return 0;
     }
     return 1;
 }
 
 /* Question A-2 */
 /****************/
-/*
-void deleteZeros() {
+void deleteZeros(char string[]) {
     int i = 0, k = 0;
-    for (i = 0; S[i] != '\0'; ++i) {
-        if (S[i] != 48) break;
+    for (i = 0; string[i] != '\0'; ++i) {
+        if (string[i] != 48) break;
     }
-    if (i < strlen(S)) {
-        for (k = 0; S[i] != '\0'; ++k) {
-            S[k] = S[i];
+    if (i < strlen(string)) {
+        for (k = 0; string[i] != '\0'; ++k) {
+            string[k] = string[i];
             i++;
         }
-        S[k] = '\0';
+        string[k] = '\0';
     } else {
-        S[k] = '0';
-        S[k+1] = '\0';
+        string[k] = '0';
+        string[k+1] = '\0';
     }
 }
-*/
 
 /* Question A-3 */
 /****************/
--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include "FirstProblem.h"
 
 int main() {
-    char Sum[] = "76087;";
+    char Sum[] = "76087";
     printf("%d ", stringNumbers(Sum));
     // adder("9843","33",Sum);
 

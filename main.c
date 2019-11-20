eb9e1822142e4305659d03238038dc5f2ff426bc
Mehdi <amigoinpt@gmail.com>
2019-11-20 23:57:15 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "FirstProblem.h"
+
+int main() {
+    char Sum[] = "0000000";
+    deleteZeros(Sum);
+    printf("%s ", Sum);
+    // adder("9843","33",Sum);
+
+    return 0;
+}

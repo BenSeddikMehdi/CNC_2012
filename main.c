b6c70a4bfdbeea517d42144642cea7b45dcf894b
BenSeddikMehdi <amigoinpt@gmail.com>
2019-11-25 19:47:52 +0100

    Finishing the task of

--- main.c
+++ main.c
@@ -2,10 +2,9 @@
 #include "FirstProblem.h"
 
 int main() {
-    char *ans;
+    char sum[] = "";
     //adder("9843","19",Sum);
-    ans = do_ptr(add,"123","324");
-    printf("Sum = %s\n", ans);
-
+    do_ptr(add,"999655", "1122638", sum);
+    printf("Sum = %s\n", sum);
     return 0;
 }

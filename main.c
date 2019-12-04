7fab9c1404f28bfdc0bdb5e84072e00b902b49a6
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-04 19:12:35 +0100

    Working on the second problem of the National Contest of 2012

--- main.c
+++ main.c
@@ -2,13 +2,13 @@
 #include "FirstProblem.h"
 #include "SecondProblem.h"
 int main() {
-    char string[] = "60789767009";
+    char string[] = "00663470789767009";
     NumberList_t *test = NULL;
     printf("the address of the first element = 0x%p\n", test);
     test = gatherNumbers(string);
     printf("the address of the first element = 0x%p\n", test);
     printStruct(test);
-    free(test);
     printf("the address of the first element = 0x%p\n", test);
+    free(test);
     return 0;
 }
